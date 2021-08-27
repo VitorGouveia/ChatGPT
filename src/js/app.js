087fa299336b1079460378cb148e09d770078d7e
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-27 03:26:45 -0300

    fix: footer position

--- src/js/app.js
+++
@@ -1,5 +0,0 @@
-export const app = () => {
-  return {
-    step: 1,
-  }
-}
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,5 @@
 import { masker } from "./masker.js"
-import { app } from "./app.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   masker()
 })
-
-export { app }
