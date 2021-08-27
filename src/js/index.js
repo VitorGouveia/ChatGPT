6e46f52fcc99f93eec0a6f8303b8cc72e6b10e47
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-27 06:06:56 -0300

    feat: add AngularJS for Form handling + doc creation

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,36 @@ import { masker } from "./masker.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   masker()
+
+  var app = angular.module('app',[]);
+
+  app.controller('formController', ($scope) =>{
+    $scope.formData = {
+      name: "",
+      work: "",
+      country: "",
+      relation: "",
+      socialSecurity: ""
+    }
+
+    $scope.showModal = false
+    $scope.showDoc = false
+    
+    $scope.setInput = (event, value) => {
+      $scope.formData[value] = event.formData[value]
+    }
+    
+    $scope.disableModal = () => {
+      $scope.showModal = false
+      $scope.showDoc = true
+    }
+    
+    $scope.createDoc = (event) => {
+      $scope.showModal = true
+      console.log($scope.formData)
+    }
+    //codigo aqui
+
+    console.log($scope.formData)
+  });
 })
